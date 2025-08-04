92abffa8e9065eef1e04a7a5a6106f42e511a5f1
Will Adams <bushblade@gmail.com>
2025-08-04 20:43:57 +0100

    example complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import "./App.css";
 import Slider from "react-touch-drag-slider";
 
+const images = [
+  {
+    url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/random-number-generation-in-js.avif",
+    title: "imageOne",
+  },
+  {
+    url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/javascript-closures.avif",
+    title: "imageTwo",
+  },
+  {
+    url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/my-first-browser-extension.avif",
+    title: "imageThree",
+  },
+  {
+    url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/add-eslint-to-vite.avif",
+    title: "imageFour",
+  },
+  {
+    url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/bad-response-with-fetch.avif",
+    title: "imageFive",
+  },
+];
+
 function App() {
-  const images = [
-    {
-      url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/random-number-generation-in-js.avif",
-      title: "imageOne",
-    },
-    {
-      url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/javascript-closures.avif",
-      title: "imageTwo",
-    },
-    {
-      url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/my-first-browser-extension.avif",
-      title: "imageThree",
-    },
-    {
-      url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/add-eslint-to-vite.avif",
-      title: "imageFour",
-    },
-    {
-      url: "https://res.cloudinary.com/bushblade/image/upload/ar_16:9,c_fill,w_800/v1/bushbladedotdev/bad-response-with-fetch.avif",
-      title: "imageFive",
-    },
-  ];
   return (
     <main>
       <Slider>
